5b1a2aa506639dd1a7b3d19d0d61158e1c09035b
khk4912 <30457148+khk4912@users.noreply.github.com>
2025-07-12 17:37:17 +0900

    🐛 fix: Fix element replacement logic in setMaxHQ function

--- src/utils/max_hq.ts
+++ src/utils/max_hq.ts
@@ -1,12 +1,10 @@
 export async function setMaxHQ (): Promise<void> {
   const original = await waitForElement('.pzp-setting-quality-pane__list-container > ul > li') as HTMLLIElement
+  const parent = original.parentElement
 
-  const temp = original.cloneNode(true) as HTMLLIElement
-  original.replaceWith(temp)
+  const clone = original.cloneNode(true) as HTMLLIElement
+  parent?.replaceChild(clone, original)
 
-  temp.click()
-
-  requestAnimationFrame(() => {
-    temp.replaceWith(original)
-  })
+  original.click()
+  requestAnimationFrame(() => parent?.replaceChild(original, clone))
 }
