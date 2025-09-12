7ec66d684066eaf664712ba5ab60a6d74b45ace1
khk4912 <30457148+khk4912@users.noreply.github.com>
2025-09-12 16:08:35 +0900

    🐛 fix: Update selector for waitForElement in setMaxHQ

--- src/utils/max_hq.ts
+++ src/utils/max_hq.ts
@@ -1,5 +1,5 @@
 export async function setMaxHQ (): Promise<void> {
-  const original = await waitForElement('.pzp-setting-quality-pane__list-container > ul > li') as HTMLLIElement
+  const original = await waitForElement('ul.pzp-setting-quality-pane__list-container > li') as HTMLLIElement
   const parent = original.parentElement
 
   const clone = original.cloneNode(true) as HTMLLIElement
